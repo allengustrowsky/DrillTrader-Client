4eb2373f2e81f8416422c6217ce5e1fc89762ed4
Allen Gustrowsky <allen@may21.us>
2023-04-28 15:21:42 -0400

    Added dashboard, sample asset/transaction classes

---
+++ Data/Asset.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace client.Data
+{
+	public struct Asset
+	{
+		public Asset(int id, string name, string ticker_symbol, int asset_type)
+		{
+			Id = id;
+			Name = name;
+			Symbol = ticker_symbol;
+			AssetType = asset_type;
+		}
+
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string Symbol { get; set; }
+		public int AssetType { get; set; }
+    }
+}
+
