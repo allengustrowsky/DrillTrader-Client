59c56c0d39fb0001413be81d327cd7f4d8553b65
Allen Gustrowsky <allen@may21.us>
2023-04-30 15:07:07 -0400

    Price data is properly fetched from server

---
+++ Data/AssetPriceData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace client.Data
+{
+    // Definition adapted from json2csharp.com
+    // RooAssetPriceDatat myDeserializedClass = JsonConvert.DeserializeObject<AssetPriceData>(myJsonResponse);
+    public class AssetPriceData
+    {
+        public string name { get; set; }
+        public double currentPrice { get; set; }
+        public long time { get; set; }
+        public string assetType { get; set; }
+    }
+}
+
---
+++ Data/PriceData.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace client.Data
+{
+	public class PriceData
+	{
+		public static Dictionary<string, AssetPriceData> Data { get; set; } = new Dictionary<string, AssetPriceData>();
+	}
+}
+
