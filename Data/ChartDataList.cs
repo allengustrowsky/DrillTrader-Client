3fa6ede0d24f243cb15d727d6e18960c74dc8da7
Allen Gustrowsky <allen@may21.us>
2023-05-01 00:31:40 -0400

    Line chart buggy but passable, trade view working

---
+++ Data/ChartDataList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace client.Data
+{
+	public class ChartDataList
+	{
+        //public ChartDataList()
+        //{
+        //}
+        //public static List<ChartDataItem> Data { get; set; } = new List<ChartDataItem>();
+        public static List<double> Data { get; set; } = new List<double>();
+        public static List<string> Labels { get; set; } = new List<string>();
+    }
+}
+
