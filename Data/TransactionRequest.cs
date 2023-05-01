1f0d284aea32b20979fc53cf3bbe5359cf416c75
Allen Gustrowsky <allen@may21.us>
2023-05-01 04:52:14 -0400

    Transactions fully functional

---
+++ Data/TransactionRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace client.Data
+{
+    // this definition adapted from json2csharp.com
+	public class TransactionRequest
+	{
+        // TransactionRequest myDeserializedClass = JsonConvert.DeserializeObject<TransactionRequest>(myJsonResponse);
+        public int portfolio_id { get; set; }
+        public int asset_id { get; set; }
+        public double units { get; set; }
+        public bool is_buy { get; set; }
+
+    }
+}
+
